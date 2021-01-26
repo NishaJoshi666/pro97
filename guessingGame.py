1161ce21f0147f8d7b30189cb509680716913018
NishaJoshi666 <69041055+NishaJoshi666@users.noreply.github.com>
2021-01-26 13:37:28 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,45 @@
+Number = 'Number Guessing Game'
+print(Number)
+Digit = 'Guess a Number.'
+print(Digit)
+# num. = '20'
+
+digit = int(input('1.Enter Your Guess:- '))
+if(digit<4):
+  print("Your guess was too low : Guess a number higher than this number")
+elif(digit<21):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+number = int(input('2.Enter Your Guess:- '))
+if(number<4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(number<21):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+nisha = int(input('3.Enter Your Guess:- '))
+if(nisha>4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(nisha<21):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+nj = int(input('4.Enter Your Guess:- '))
+if(nj>4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(nj<21):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+joshi = int(input('5.Enter Your Guess:- '))
+if(joshi>4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(joshi<21):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
